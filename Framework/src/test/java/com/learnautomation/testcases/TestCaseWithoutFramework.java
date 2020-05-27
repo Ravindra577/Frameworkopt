6144ea9aed5eb1be8597673c9ef734a7ccee9e37
kavyasree <kavya.panchumarthy@gmail.com>
2020-05-27 12:47:35 +0530

    added all content

---
+++ Framework/src/test/java/com/learnautomation/testcases/TestCaseWithoutFramework.java
@@ -0,0 +1,52 @@
+package com.learnautomation.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestCaseWithoutFramework {
+
+	@Test
+	public void test1() throws InterruptedException 
+	{
+		System.setProperty("webdriver.Firefox.driver","/usr/local/bin/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
+	
+		driver.get("http://freecrm.com/");
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.name("username")).sendKeys("Selenium_50");
+
+		driver.findElement(By.name("password")).sendKeys("Abcd@123456");
+
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+		driver.quit();
+	}
+@Test
+	public void test02Chrome() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://freecrm.com/");
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.name("username")).sendKeys("Selenium_50");
+
+		driver.findElement(By.name("password")).sendKeys("Abcd@123456");
+
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+		driver.quit();
+	}
+}
+
